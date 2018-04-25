d917e8f994dea83405ba0e4ba1202dacbae2d394
Matt McGinnis <37253411+mamcginn@users.noreply.github.com>
2018-04-24 21:29:04 -0400

    Added setup script and commit script.

---
+++ setup-git-v1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+git init
+git config --global user.name "Matt McGinnis"
+git config --global user.email "mcginnisma90@gmail.com"
+sudo git config --system core.editor vim
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=1000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
