29114981e4b897c92304a7ec858b6ed39e13e2b1
Matt McGinnis <mcginnisma90@gmail.com>
2018-04-25 09:57:05 -0400

    Updated and added installation and configuration scripts.

---
+++ 300-Configuration-File-Setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##################################################
+########### Configuration Setup Script ###########
+##################################################
+
+# Set time to local-time (Dual Boot with Windows)
+sudo timedatectl set-local-rtc 1 --adjust-system-clock
+
+# Replace tlp with personal configuration (SOUND_POWER_SAVE_ON_BAT=0)
+sudo rm /etc/default/tlp
+sudo cp ~/GitHub/Manjaro-Linux-Cinnamon/etc/default/tlp /etc/default/
