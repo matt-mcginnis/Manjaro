e94e7b3d9fd8cdd42434a2e6001461f63cd902b2
Matt McGinnis <mcginnisma90@gmail.com>
2018-04-25 09:17:07 -0400

    Wrote installation script to install i3.

---
+++ 100-Manjaro-Package-Installation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+##################################################
+########### Configuration Setup Script ###########
+##################################################
+
+# It is important to run sudo pacman -Syu and 
+# reboot after a fresh installation. This script 
+# is intended to be run directly after a fresh 
+# installation and reboot.
+
+########################################
+#### Loading Mirrors and Updating  #####
+########################################
+
+# Repalace pacman.conf with personal configuration
+sudo rm /etc/pacman.conf
+sudo cp ~/GitHub/Manjaro-Linux-Cinnamon/etc/pacman.conf /etc/
+
+# Create mirror list
+sudo pacman-mirrors --geoip
+
+# Update repositories and packages
+sudo pacman -Syyu
+
+################################
+##### Package Installation #####
+################################
+
+# Install dmenu
+sudo pacman -S dmenu
+
+# Install i3-gaps, i3status, i3blocks, i3lock
+sudo pacman -S i3-gaps i3blocks i3lock
