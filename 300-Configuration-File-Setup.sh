794179e03e74d458c7ffa792c11d9744d4a2e9e3
Matt McGinnis <mcginnisma90@gmail.com>
2018-04-29 11:01:13 -0400

    Removed nonfree video drivers.

--- 300-Configuration-File-Setup.sh
+++ 300-Configuration-File-Setup.sh
@@ -7,9 +7,6 @@
 # Set time to local-time (Dual Boot with Windows)
 sudo timedatectl set-local-rtc 1 --adjust-system-clock
 
-# Install nvidia proprietary drivers
-sudo mhwd -a -pci nonfree 0300
-
 # Replace tlp with personal configuration (SOUND_POWER_SAVE_ON_BAT=0)
 sudo rm /etc/default/tlp
 sudo cp ~/GitHub/Manjaro-Linux-Cinnamon/etc/default/tlp /etc/default/
