76d948a4cdb39675332201d13765aac9e6d15d84
Matt McGinnis <mcginnisma90@gmail.com>
2018-05-07 20:10:56 -0400

    Updated ruby installation.

--- 100-Manjaro-Package-Installation.sh
+++ 100-Manjaro-Package-Installation.sh
@@ -57,9 +57,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io -o rvm.sh
 cat rvm.sh | bash -s stable
 source ~/.rvm/scripts/rvm
-rvm install 2.4
-rvm use 2.4
-rvm --default use 2.4
+rvm install ruby --default
 gem install rails
 
 # Install sysstat
