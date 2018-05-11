75a4031be41ebf476341452f7c523be109993cc9
Matt McGinnis <mcginnisma90@gmail.com>
2018-05-11 14:29:59 -0400

    Updated ruby installation code.

--- 100-Manjaro-Package-Installation.sh
+++ 100-Manjaro-Package-Installation.sh
@@ -55,6 +55,7 @@ sudo pacman -S ranger
 # Install ruby
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
 rvm install 2.4
 rvm use 2.4
 rvm --default use 2.4
