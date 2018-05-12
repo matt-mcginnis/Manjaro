696807f7007676a295093a3e634a8a70d3bb51ff
Matt McGinnis <mcginnisma90@gmail.com>
2018-05-12 18:43:55 -0400

    Added code for installation of SSH keys for GitHub and Heroku.

--- 300-GitHub-Package-Installation.sh
+++ 300-GitHub-Package-Installation.sh
@@ -6,3 +6,7 @@
 
 # Install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install heroku toolbelt
+curl https://cli-assets.heroku.com/install-standalone.sh | sh
+
--- 400-Configuration-File-Setup.sh
+++ 400-Configuration-File-Setup.sh
@@ -39,3 +39,11 @@ cp ~/GitHub/Manjaro-Linux-Cinnamon/home/.vimrc ~/
 # Copy .bashrc personal configuration
 sudo rm ~/.bashrc
 cp ~/GitHub/Manjaro-Linux-Cinnamon/home/.bashrc ~/
+
+### SSH key setup ###
+ssh-keygen -C mcginnisma90@gmail.com -t rsa
+
+# Be sure to delete the old linux key and replace it on GitHub
+
+# Be sure to delete the old key on Heroku
+heroku keys:add
