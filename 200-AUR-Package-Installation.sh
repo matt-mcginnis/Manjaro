aeae40069cf671d9b8a37a89bb10af673179e488
Matt McGinnis <mcginnisma90@gmail.com>
2018-05-14 19:55:41 -0400

    Installed gnome-terminal-transparency.

--- 200-AUR-Package-Installation.sh
+++ 200-AUR-Package-Installation.sh
@@ -4,5 +4,8 @@
 ########## AUR Package Installation ##########
 ##############################################
 
+# Install gnome-terminal-transparency
+yaourt gnome-terminal-transparency
+
 # Install google chrome
 yaourt google-chrome
