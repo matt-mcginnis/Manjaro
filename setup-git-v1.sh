d1e1c7d9bbb0c0d6c4d82694aa5e361064e52ad7
Matt McGinnis <mcginnisma90@gmail.com>
2018-05-22 16:29:59 -0400

    Updated setup-git-v1.sh to hold credentials for an hour.

--- setup-git-v1.sh
+++ setup-git-v1.sh
@@ -11,7 +11,7 @@ git config --global user.name "Matt McGinnis"
 git config --global user.email "mcginnisma90@gmail.com"
 sudo git config --system core.editor vim
 git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=1000'
+git config --global credential.helper 'cache --timeout=3600'
 git config --global push.default simple
 
 
