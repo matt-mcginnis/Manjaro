df6cba78c6fcd0566b08bd1ce697b1fc478e0715
Matt McGinnis <mcginnisma90@gmail.com>
2018-06-09 21:35:24 -0400

    Updated custom auto-suggestions.zsh.

--- home/.oh-my-zsh/custom/auto-suggestions.zsh
+++ home/.oh-my-zsh/custom/auto-suggestions.zsh
@@ -5,7 +5,7 @@
 # Color to use when highlighting suggestion
 # Uses format of `region_highlight`
 # More info: http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Widgets
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=2'
 
 # Prefix to use when saving original versions of bound widgets
 ZSH_AUTOSUGGEST_ORIGINAL_WIDGET_PREFIX=autosuggest-orig-
