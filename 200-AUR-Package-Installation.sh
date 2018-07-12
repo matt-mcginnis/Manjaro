2649752853121d004628a116c5e5c65b774978cb
Matt McGinnis <mcginnisma90@gmail.com>
2018-07-11 21:43:29 -0400

    Added an extra line at the bottom of 200-AUR-Package-Installation.sh

--- 200-AUR-Package-Installation.sh
+++ 200-AUR-Package-Installation.sh
@@ -49,3 +49,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # Install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
